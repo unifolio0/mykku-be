6531c961436a2bf4e2313e0a7124737e03d7523f
unifolio0 <boho82ha@gmail.com>
2025-05-03 20:06:57 +0900

    feat: 초기 세팅

---
+++ src/main/kotlin/com/example/mykku/MykkuApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mykku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MykkuApplication
+
+fun main(args: Array<String>) {
+	runApplication<MykkuApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mykku/MykkuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mykku
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MykkuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
