b5294198a4f6f23b8c00a2cbb3e3a61016a45a59
unifolio0 <boho82ha@gmail.com>
2025-05-30 13:51:42 +0900

    refactor: enum 변수에 val 추가

--- src/main/kotlin/com/example/mykku/exception/ErrorCode.kt
+++ src/main/kotlin/com/example/mykku/exception/ErrorCode.kt
@@ -2,6 +2,6 @@ package com.example.mykku.exception
 
 import org.springframework.http.HttpStatus
 
-enum class ErrorCode(status: HttpStatus, message: String) {
+enum class ErrorCode(val status: HttpStatus, val message: String) {
     NOT_FOUND_DAILY_MESSAGE(HttpStatus.NOT_FOUND, "하루 덕담을 찾을 수 없습니다"),
 }
