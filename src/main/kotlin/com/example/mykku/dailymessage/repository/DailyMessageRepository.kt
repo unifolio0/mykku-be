8804348cdea9296d7f538baee36c614cd22f6c57
unifolio0 <boho82ha@gmail.com>
2025-06-24 23:36:39 +0900

    fix: 에러 메시지 반환하도록 수정

--- src/main/kotlin/com/example/mykku/dailymessage/repository/DailyMessageRepository.kt
+++ src/main/kotlin/com/example/mykku/dailymessage/repository/DailyMessageRepository.kt
@@ -1,8 +1,6 @@
 package com.example.mykku.dailymessage.repository
 
 import com.example.mykku.dailymessage.domain.DailyMessage
-import com.example.mykku.exception.ErrorCode
-import com.example.mykku.exception.MykkuException
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
@@ -10,8 +8,4 @@ import java.time.LocalDate
 @Repository
 interface DailyMessageRepository : JpaRepository<DailyMessage, Long> {
     fun findByDate(date: LocalDate): DailyMessage?
-
-    fun getByDate(date: LocalDate): DailyMessage {
-        return findByDate(date) ?: throw MykkuException(ErrorCode.NOT_FOUND_DAILY_MESSAGE)
-    }
 }
