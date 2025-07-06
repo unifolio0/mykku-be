ac4b5d4e3a45a89903760f7394131e661112840c
unifolio0 <boho82ha@gmail.com>
2025-07-06 15:09:21 +0900

    refactor: import 수정

--- src/main/kotlin/com/example/mykku/member/tool/LikeBoardWriter.kt
+++ src/main/kotlin/com/example/mykku/member/tool/LikeBoardWriter.kt
@@ -1,6 +1,7 @@
 package com.example.mykku.member.tool
 
 import com.example.mykku.board.domain.Board
+import com.example.mykku.member.domain.LikeBoard
 import com.example.mykku.member.domain.Member
 import com.example.mykku.member.repository.LikeBoardRepository
 import org.springframework.stereotype.Component
@@ -10,7 +11,7 @@ class LikeBoardWriter(
     private val likeBoardRepository: LikeBoardRepository
 ) {
     fun createLikeBoard(member: Member, board: Board) {
-        val likeBoard = com.example.mykku.member.domain.LikeBoard(
+        val likeBoard = LikeBoard(
             member = member,
             board = board
         )
