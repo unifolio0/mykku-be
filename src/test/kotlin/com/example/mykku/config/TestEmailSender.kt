08acb25cebedadcec7ddd1f22c1611481dec8db9
unifolio0 <boho82ha@gmail.com>
2025-10-31 22:50:43 +0900

    feat: 테스트 config 타입 수정

--- src/test/kotlin/com/example/mykku/config/TestEmailSender.kt
+++ src/test/kotlin/com/example/mykku/config/TestEmailSender.kt
@@ -1,5 +1,6 @@
 package com.example.mykku.config
 
+import com.example.mykku.email.domain.VerificationPurpose
 import com.example.mykku.email.tool.EmailSender
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -12,7 +13,7 @@ class TestEmailSenderConfig {
     @Primary
     fun emailSender(): EmailSender {
         return object : EmailSender {
-            override fun sendVerificationCode(to: String, code: String, purpose: String) {
+            override fun sendVerificationCode(to: String, code: String, purpose: VerificationPurpose) {
                 // Do nothing in tests
             }
         }
