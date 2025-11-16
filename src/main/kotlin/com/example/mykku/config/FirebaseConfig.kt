dfd8fbaf978d49cc14e898e068b49d9ac7496ce0
unifolio0 <boho82ha@gmail.com>
2025-11-16 18:57:30 +0900

    feat: 테스트 세팅

--- src/main/kotlin/com/example/mykku/config/FirebaseConfig.kt
+++ src/main/kotlin/com/example/mykku/config/FirebaseConfig.kt
@@ -6,9 +6,11 @@ import com.google.firebase.FirebaseOptions
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import java.io.FileInputStream
 
 @Configuration
+@Profile("!test")
 class FirebaseConfig(
     @Value("\${firebase.service-account-key-path}")
     private val serviceAccountKeyPath: String
