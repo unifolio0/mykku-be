c61116b1f0ce36375ad7d23e5617577a0b8ccc70
unifolio0 <boho82ha@gmail.com>
2026-01-27 18:37:06 +0900

    test: 헤더 추가

--- src/test/kotlin/com/example/mykku/docs/RestDocumentationRequest.kt
+++ src/test/kotlin/com/example/mykku/docs/RestDocumentationRequest.kt
@@ -47,6 +47,7 @@ class RestDocumentationRequest {
 
     fun requestHeader(vararg descriptors: HeaderDescriptor): RestDocumentationRequest {
         snippets.add(requestHeaders(*descriptors))
+        resourceBuilder.headers(*descriptors)
         return this
     }
 
